84509df96326256adfeb346edab8fe1223d2e84f
Abel <abelberhane011@gmail.com>
2019-10-03 11:57:01 +0300

    inserts background for about class

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,31 @@ $(document).ready(function () {
         $(".contentsC").toggle();
         $(".cheese").toggle();
     });
+    $(".clickable1").click(function () {
+        $(".contentsP").toggle();
+        $(".pep").toggle();
+    });
     $(".clickable2").click(function () {
-        $(".development").toggle();
-        $(".img2-class").toggle();
+        $(".contentsM").toggle();
+        $(".mar").toggle();
     });
     $(".clickable3").click(function () {
-        $(".product").toggle();
-        $(".img3-class").toggle();
+        $(".contentsV").toggle();
+        $(".vege").toggle();
     });
-    $("form.inputs").submit(function(event) {
-    event.preventDefault();
-    var name = $(".name").val();
-    var email = $(".email").val();
-    alert(name + " we have received your message. Thank you for reaching out to us." );
+    $(".clickable4").click(function () {
+        $(".contentsE").toggle();
+        $(".exe").toggle();
+    });
+    $(".clickable5").click(function () {
+        $(".contentsH").toggle();
+        $(".special").toggle();
+    });
+    $("form.inputs").submit(function (event) {
+        event.preventDefault();
+        var name = $(".name").val();
+        var email = $(".email").val();
+        alert(name + " we have received your message. Thank you for reaching out to us.");
     });
     $(".hover-box").hover(function (event) {
         console.log(event.target.attributes.alt.value);
